4e666711ed4ed48370d113419e699184b77cc8ac
Minseo Park <hightuv@gmail.com>
2025-06-23 16:02:34 +0900

    Feat: User 엔티티 컬럼 설정 변경
    - Base Entity 도입

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,28 +1,27 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity } from 'src/common/entities/base.entity';
+import { BeforeInsert, BeforeUpdate, Column, Entity } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Entity()
-export class User {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column({
-    unique: true,
-  })
+export class User extends BaseEntity {
+  @Column({ unique: true })
   email: string;
 
   @Column()
   password: string;
 
-  @Column({
-    unique: true,
-  })
+  @Column({ unique: true })
   nickname: string;
 
-  @Column({
-    unique: true,
-  })
+  @Column({ unique: true, nullable: true })
   contact: string;
 
-  @Column()
+  @Column({ nullable: true })
   image: string;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
 }
