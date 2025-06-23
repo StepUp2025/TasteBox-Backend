7d5522237ee92c8957754e9ab83985bbc685fa0b
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-06-23 22:46:44 +0900

    Feat: Swagger 환경설정 추가
    - Swagger(OpenAPI) 문서화 도구 도입 및 /api-docs 경로 적용

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const config = new DocumentBuilder()
+    .setTitle('Movie API')
+    .setDescription('The movie API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
   const app = await NestFactory.create(AppModule);
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api-docs', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
