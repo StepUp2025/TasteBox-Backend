0bea34857a1b804ad65dcc61410e51af0c769944
Minseo Park <hightuv@gmail.com>
2025-06-23 16:26:50 +0900

    Feat: 인증 관련 Strategy 및 Guard 구현

---
+++ src/auth/strategies/google.strategy.ts
@@ -0,0 +1,54 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Profile, Strategy, VerifyCallback } from 'passport-google-oauth20';
+import googleOauthConfig from '../config/google-oauth.config';
+import { ConfigType } from '@nestjs/config';
+import { AuthService } from '../auth.service';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @Inject(googleOauthConfig.KEY)
+    private readonly googleConfiguration: ConfigType<typeof googleOauthConfig>,
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {
+    if (
+      !googleConfiguration.clientID ||
+      !googleConfiguration.clientSecret ||
+      !googleConfiguration.callbackURL
+    ) {
+      throw new Error('Google OAuth Configuration must be defined');
+    }
+
+    super({
+      clientID: googleConfiguration.clientID,
+      clientSecret: googleConfiguration.clientSecret,
+      callbackURL: googleConfiguration.callbackURL,
+      scope: ['email', 'profile'],
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+    done: VerifyCallback,
+  ) {
+    console.log({ profile });
+    const { emails, name } = profile;
+
+    if (!emails || emails.length === 0) {
+      throw new Error('Google 프로필에 이메일 정보가 없습니다.');
+    }
+
+    const user = await this.authService.validateGoogleUser({
+      email: emails[0].value,
+      nickname: await this.userService.generateUniqueNickname(name?.givenName),
+      password: '',
+    });
+
+    done(null, user);
+  }
+}
