f855aada97712afa2d439d73dbc25c08080323ee
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-07-09 08:36:51 +0900

    Feat: 영화/TV 시리즈 상태 Enum 추가

---
+++ src/content/enum/tv-series-status.enum.ts
@@ -0,0 +1,8 @@
+export enum TvSeriesStatus {
+  RETURNING_SERIES = 'Returning Series', // 방영 중인 시리즈
+  PLANNED = 'Planned', // 기획됨
+  IN_PRODUCTION = 'In Production', // 제작 중
+  ENDED = 'Ended', // 종영됨
+  CANCELED = 'Canceled', // 취소됨
+  PILOT = 'Pilot', // 시험적으로 제작
+}
