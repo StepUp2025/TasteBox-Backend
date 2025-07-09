9079ec66ff8487c968f3c05738f72417499a7463
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-07-10 05:36:09 +0900

    Refactor: 총 페이지 수 계산 생성자 추가
    - DTO 내부에서 totalPages를 계산하도록 수정

--- src/content/movie/dto/movie-list-response.dto.ts
+++ src/content/movie/dto/movie-list-response.dto.ts
@@ -1,7 +1,15 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { Expose, Type } from 'class-transformer';
+import { Expose, plainToInstance, Type } from 'class-transformer';
 import { ContentType } from 'src/common/enums/content-type.enum';
+import { Movie } from './../entities/movie.entity';
 import { MovieListItemDto } from './movie-list-item.dto';
+
+interface MovieListResponseDtoParams {
+  results: Movie[];
+  page: number;
+  limit: number;
+  totalCount: number;
+}
 
 export class MovieListResponseDto {
   @Expose()
@@ -27,4 +35,13 @@ export class MovieListResponseDto {
   @Expose()
   @ApiProperty({ description: '총 페이지 수', example: 1000 })
   totalPages: number;
+
+  constructor(params: MovieListResponseDtoParams) {
+    this.contentType = ContentType.MOVIE;
+    this.page = params.page;
+    this.totalPages = Math.ceil(params.totalCount / params.limit);
+    this.movies = plainToInstance(MovieListItemDto, params.results, {
+      excludeExtraneousValues: true,
+    });
+  }
 }
