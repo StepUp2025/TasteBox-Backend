600b1005fdf3f3b3febe7362a886c6155cb4d473
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-07-10 18:03:41 +0900

    Fix: 인스턴스 이름 오타 수정
    - tvSeroesRepository에서 tvSeriesRepository로 변경

--- src/content/tv-series/tv-series.service.ts
+++ src/content/tv-series/tv-series.service.ts
@@ -4,12 +4,12 @@ import { TvSeriesListResponseDto } from './dto/tv-series-list-response.dto';
 import { TvSeriesRepository } from './repository/tv-series.repository';
 @Injectable()
 export class TvSeriesService {
-  constructor(private readonly tvSeroesRepository: TvSeriesRepository) {}
+  constructor(private readonly tvSeriesRepository: TvSeriesRepository) {}
 
   // TV 시리즈 상세 정보 조회
   async findTvSeriesById(id: number): Promise<TvSeriesDetailResponseDto> {
     const tvSeriesContent =
-      await this.tvSeroesRepository.getTvSeriesDetailById(id);
+      await this.tvSeriesRepository.getTvSeriesDetailById(id);
     const tvDetailDto = new TvSeriesDetailResponseDto(tvSeriesContent);
     return tvDetailDto;
   }
@@ -20,7 +20,7 @@ export class TvSeriesService {
     limit: number = 20,
   ): Promise<TvSeriesListResponseDto> {
     const [results, totalCount] =
-      await this.tvSeroesRepository.findOnTheAirTvSeries(page, limit);
+      await this.tvSeriesRepository.findOnTheAirTvSeries(page, limit);
     return new TvSeriesListResponseDto({
       results,
       totalCount,
@@ -35,7 +35,7 @@ export class TvSeriesService {
     limit: number = 20,
   ): Promise<TvSeriesListResponseDto> {
     const [results, totalCount] =
-      await this.tvSeroesRepository.findPopularTvSeries(page, limit);
+      await this.tvSeriesRepository.findPopularTvSeries(page, limit);
     return new TvSeriesListResponseDto({
       results,
       totalCount,
@@ -50,7 +50,7 @@ export class TvSeriesService {
     limit: number = 20,
   ): Promise<TvSeriesListResponseDto> {
     const [results, totalCount] =
-      await this.tvSeroesRepository.findTopRatedTvSeries(page, limit);
+      await this.tvSeriesRepository.findTopRatedTvSeries(page, limit);
     return new TvSeriesListResponseDto({
       results,
       totalCount,
@@ -66,7 +66,7 @@ export class TvSeriesService {
     limit: number = 20,
   ): Promise<TvSeriesListResponseDto> {
     const [results, totalCount] =
-      await this.tvSeroesRepository.findTvSeriesByGenreIds(
+      await this.tvSeriesRepository.findTvSeriesByGenreIds(
         genreIds,
         page,
         limit,
@@ -86,7 +86,7 @@ export class TvSeriesService {
     limit: number = 20,
   ): Promise<TvSeriesListResponseDto> {
     const [results, totalCount] =
-      await this.tvSeroesRepository.findRecommendTvSeriesById(
+      await this.tvSeriesRepository.findRecommendTvSeriesById(
         movieId,
         page,
         limit,
