43f0448beecf4a002bdd1fe308bb59f69dcdffd0
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-07-11 18:44:10 +0900

    Feat: 회원 취향 DTO 및 API 응답 구조 개선
    - genres 필드 타입 GenreDto[] 에서 GenreWithEmojiDto[]로 변경

--- src/preference/dto/response/preference-detail.dto.ts
+++ src/preference/dto/response/preference-detail.dto.ts
@@ -1,16 +1,42 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { GenreDto } from 'src/genre/dto/genre.dto';
+import { ContentType } from 'src/common/enums/content-type.enum';
+import { Genre } from 'src/genre/entities/genre.entity';
+import { GenreWithEmojiDto } from './../../../genre/dto/genre-with-emoji.dto';
+import { Preference } from './../../entities/preference.entity';
 
 export class PreferenceDetailDto {
   @ApiProperty({
     description: '선호 장르 목록',
-    type: [GenreDto],
+    type: [GenreWithEmojiDto],
   })
-  genres: GenreDto[];
+  genres: GenreWithEmojiDto[];
 
   @ApiProperty({
     description: '선호 장르 개수',
     example: 3,
   })
   count: number;
+
+  constructor(genres: GenreWithEmojiDto[], count: number) {
+    this.genres = genres;
+    this.count = count;
+  }
+
+  static of(
+    preferences: Preference[],
+    contentType: ContentType,
+  ): PreferenceDetailDto {
+    const filteredAndMappedGenres: GenreWithEmojiDto[] = preferences
+      .filter((p) => p.genre && p.genre.type === contentType)
+      .map((p) => {
+        const genre = p.genre as Genre;
+        return GenreWithEmojiDto.ofWithEmoji(genre, contentType);
+      })
+      .sort((a, b) => a.id - b.id);
+
+    return new PreferenceDetailDto(
+      filteredAndMappedGenres,
+      filteredAndMappedGenres.length,
+    );
+  }
 }
