1d2b541795027e6a270c71693df3f49cd2afdc8d
Minseo Park <hightuv@gmail.com>
2025-07-12 22:41:58 +0900

    Feat: Collection 모듈 설정 및 불필요한 모듈 제거

--- src/collection/collection.module.ts
+++ src/collection/collection.module.ts
@@ -1,4 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { S3Module } from 'src/common/aws/s3.module';
+import { Content } from 'src/content/entities/content.entity';
+import { UserModule } from 'src/user/user.module';
+import { CollectionController } from './collection.controller';
+import { CollectionRepository } from './collection.repository';
+import { CollectionService } from './collection.service';
+import { Collection } from './entities/collection.entity';
+import { CollectionContent } from './entities/collection-content.entity';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Collection, CollectionContent, Content]),
+    UserModule,
+    S3Module,
+  ],
+  controllers: [CollectionController],
+  providers: [CollectionService, CollectionRepository],
+})
 export class CollectionModule {}
