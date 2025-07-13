aa2ce845e17cb6876eca9433cd16fedef85024c3
Minseo Park <hightuv@gmail.com>
2025-07-14 05:39:11 +0900

    Fix: 업데이트 시에도 닉네임이 필수 항목이도록 설정

--- src/user/dto/request/update-user-request.dto.ts
+++ src/user/dto/request/update-user-request.dto.ts
@@ -1,6 +1,13 @@
-import { PartialType, PickType } from '@nestjs/swagger';
+import { ApiProperty, PartialType, PickType } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
 import { CreateUserRequestDto } from './create-user-request.dto';
 
 export class UpdateUserProfileRequestDto extends PartialType(
   PickType(CreateUserRequestDto, ['nickname', 'contact', 'image'] as const),
-) {}
+) {
+  @ApiProperty({ description: '닉네임', example: '쌈뽕한닉네임' })
+  @IsNotEmpty({ message: '닉네임을 입력해주세요.' })
+  @IsString()
+  @Length(2, 10, { message: '닉네임은 2~10자 사이로 입력해주세요.' })
+  nickname: string;
+}
