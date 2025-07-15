ef58f725db7431ba7ad783e271595cffafd33115
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-07-16 05:29:19 +0900

    Feat: 서비스 계층 공통 페이징 로직 적용
    - MovieService, TvSeriesService에서 중복되는 페이지 유효성 검증 및 DTO 생성 로직을 제거하고 유틸함수를 활용하도록 변경
    - MovieListResponseDto, TvSeriesListResponseDto의 생성자가 totalPages를 직접 받도록 변경

--- src/content/tv-series/tv-series.service.ts
+++ src/content/tv-series/tv-series.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InvalidGenreIdException } from 'src/common/exceptions/invalid-genre-id.exception';
+import { validatePageAndCreateListDto } from './../../common/utils/pagination.util';
+import { GenreRepository } from './../../genre/repository/genre.repository';
 import { TvSeriesDetailResponseDto } from './dto/tv-series-detail-response.dto';
 import { TvSeriesListResponseDto } from './dto/tv-series-list-response.dto';
 import { TvSeriesRepository } from './repository/tv-series.repository';
+
 @Injectable()
 export class TvSeriesService {
-  constructor(private readonly tvSeriesRepository: TvSeriesRepository) {}
+  constructor(
+    private readonly tvSeriesRepository: TvSeriesRepository,
+    private readonly genreRepository: GenreRepository,
+  ) {}
 
   // TV 시리즈 상세 정보 조회
   async findTvSeriesById(id: number): Promise<TvSeriesDetailResponseDto> {
@@ -14,49 +21,56 @@ export class TvSeriesService {
     return tvDetailDto;
   }
 
-  // 방영 중인 TV 시리즈 조회
+  // 방영 중인 TV 시리즈 목록 조회
   async getOnTheAirTvSeries(
     page: number = 1,
     limit: number = 20,
   ): Promise<TvSeriesListResponseDto> {
     const [results, totalCount] =
       await this.tvSeriesRepository.findOnTheAirTvSeries(page, limit);
-    return new TvSeriesListResponseDto({
-      results,
-      totalCount,
+    return validatePageAndCreateListDto(
+      TvSeriesListResponseDto,
       page,
       limit,
-    });
+      results,
+      totalCount,
+    );
   }
 
-  // 인기 있는 TV 시리즈 조회
+  // 인기 있는 TV 시리즈 목록 조회
   async getPopularTvSeries(
     page: number = 1,
     limit: number = 20,
   ): Promise<TvSeriesListResponseDto> {
-    const [results, totalCount] =
-      await this.tvSeriesRepository.findPopularTvSeries(page, limit);
-    return new TvSeriesListResponseDto({
-      results,
-      totalCount,
+    const [results, totalCount] = await this.tvSeriesRepository.findPopular(
       page,
       limit,
-    });
+    );
+    return validatePageAndCreateListDto(
+      TvSeriesListResponseDto,
+      page,
+      limit,
+      results,
+      totalCount,
+    );
   }
 
-  // 높은 평점 TV 시리즈 조회
+  // 높은 평점 TV 시리즈 목록 조회
   async getTopRatedTvSeries(
     page: number = 1,
     limit: number = 20,
   ): Promise<TvSeriesListResponseDto> {
-    const [results, totalCount] =
-      await this.tvSeriesRepository.findTopRatedTvSeries(page, limit);
-    return new TvSeriesListResponseDto({
-      results,
-      totalCount,
+    const [results, totalCount] = await this.tvSeriesRepository.findTopRated(
       page,
       limit,
-    });
+    );
+    return validatePageAndCreateListDto(
+      TvSeriesListResponseDto,
+      page,
+      limit,
+      results,
+      totalCount,
+    );
   }
 
   // 특정 장르 TV 시리즈 조회
@@ -65,21 +79,30 @@ export class TvSeriesService {
     page: number = 1,
     limit: number = 20,
   ): Promise<TvSeriesListResponseDto> {
+    const existingGenres = await this.genreRepository.getGenresByIds(genreIds);
+    const existingGenreIds = existingGenres.map((genre) => genre.id);
+    const invalidIds = genreIds.filter((id) => !existingGenreIds.includes(id));
+
+    if (invalidIds.length > 0) {
+      throw new InvalidGenreIdException(invalidIds);
+    }
     const [results, totalCount] =
       await this.tvSeriesRepository.findTvSeriesByGenreIds(
         genreIds,
         page,
         limit,
       );
-    return new TvSeriesListResponseDto({
-      results,
-      totalCount,
+
+    return validatePageAndCreateListDto(
+      TvSeriesListResponseDto,
       page,
       limit,
-    });
+      results,
+      totalCount,
+    );
   }
 
-  // 추천 TV 시리즈 조회
+  // 추천 TV 시리즈 목록 조회
   async getRecommendedTvSeriesById(
     movieId: number,
     page: number = 1,
@@ -91,11 +114,12 @@ export class TvSeriesService {
         page,
         limit,
       );
-    return new TvSeriesListResponseDto({
-      results,
-      totalCount,
+    return validatePageAndCreateListDto(
+      TvSeriesListResponseDto,
       page,
       limit,
-    });
+      results,
+      totalCount,
+    );
   }
 }
